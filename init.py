ab92d00d69386fa94d90995f73b43b461816347a
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2019-03-29 16:25:59 +0530

    Documentation added

--- init.py
+++ init.py
@@ -1,14 +1,34 @@
+# Copyright (C) by
+# Sayan Naskar <nasccarsayan@iitkgp.ac.in>
+# Authors: Sayan Naskar <nasccarsayan@iitkgp.ac.in>
+"""
+Simulate the Tomasulo Algorithm.
+Specifications: ./assgnSpec.pdf
+"""
 #%%
 import json
 import traceback
 from collections import defaultdict
 from tabulate import tabulate
 
-NREGS = 8
+NREGS = 8  # number of registers
+
 clin = lambda: input().strip()
+""" Returns the stripped version of input() """
 remNone = lambda x: map(lambda y: '' if y is None or False else y, x)
+""" Returns an array containing the elements of the input array 
+except for 'None' elements which are converted to empty strings """
+
 
 def printState(rsg, rat, iq):
+  """Prints the current state of the hardware
+  
+  Parameters
+  ----------
+  rsg: The Reservation Station Group
+  rat: The RAT table
+  iq: The Instruction Queue
+  """
   print(' ### RESERVATION STATION ###')
   print(rsg)
   print(' ### RAT ###')
@@ -16,7 +36,23 @@ def printState(rsg, rat, iq):
   print(' ### INSTRUCTION QUEUE ###')
   print(iq)
 
+
 class InstrQ:
+  """The Instruction Queue Class
+
+  Fields
+  ______
+  queue: The list containing the instructions
+  funcmap: The dict containg the opcode -> function-name mapping
+
+  Methods
+  -------
+  push: Push an instruction into the queue
+  pop: pop the oldest instruction from the queue
+  peek: check the topmost instruction
+  isEmpty: check if the queue is empty
+
+  """
 
   def __init__(self):
     self.queue = []
@@ -33,18 +69,34 @@ class InstrQ:
 
   def isEmpty(self):
     return not self.queue
-  
+
   def __repr__(self):
     headers = ['OPCODE', 'DST OP', 'SRC OP1', 'SRC OP2']
     table = []
     for instr in self.queue:
       row = [self.funcmap[instr[0]]]
-      row.extend(['RS%d' %i for i in instr[1:]])
+      row.extend(['RS%d' % i for i in instr[1:]])
       table.append(row)
     return tabulate(table, headers, tablefmt='fancy_grid')
 
+  def __str__(self):
+    return repr(self)
+
 
 class RF:
+  """The Register File Class
+
+  Fields
+  ______
+  size: The size of the register file
+  regFile: The list containing the registers
+
+  Methods
+  -------
+  setr: Set the value of a register
+  getr: Get the value of a register
+
+  """
 
   def __init__(self, size=8):
     self.size = size
@@ -58,6 +110,21 @@ class RF:
 
 
 class RAT:
+  """The Register Alias Table
+
+  Fields
+  ______
+  size: The size of the RAT
+  regFile: The list containing the aliases to registers
+  rf: The register file this RAT points to
+
+  Methods
+  -------
+  setr: Set the value of a RAT cell
+  getr: Get the value of a RAT cell
+  clear: Clear the cells of RAT with alias matching with the broadcasted value 
+
+  """
 
   def __init__(self, rf, size=8):
     self.size = size
@@ -86,14 +153,31 @@ class RAT:
 
   def __repr__(self):
     headers = ['#', 'RF', 'RAT']
-    table = [remNone([i, self.rf.getr(i), 'RS%d' % reg if reg is not None else reg]) for (i, reg) in enumerate(self.regFile)]
+    table = [
+        remNone([i,
+                 self.rf.getr(i),
+                 'RS%d' % reg if reg is not None else reg])
+        for (i, reg) in enumerate(self.regFile)
+    ]
     return tabulate(table, headers, tablefmt='fancy_grid')
-  
+
   def __str__(self):
     return repr(self)
 
 
 class Bus:
+  """The Bus connecting the 2 ALUs to the RAT and Reservation Station Group
+
+  Fields
+  ______
+  data: A dict specifying the tag and the value produced by the ALU
+
+  Methods
+  -------
+  getb: Get the data present in the bus
+  setb: Set the data present in the bus
+
+  """
 
   def __init__(self):
     self.data = {'rs': None, 'val': None}
@@ -105,13 +189,34 @@ class Bus:
     self.data = data
 
   def __repr__(self):
-    return tabulate([remNone([self.data['rs'], self.data['val']])], ['RS', 'VAL'], tablefmt='fancy_grid')
+    return tabulate([remNone([self.data['rs'], self.data['val']])],
+                    ['RS', 'VAL'],
+                    tablefmt='fancy_grid')
 
   def __str__(self):
     return repr(self)
 
 
 class ALU:
+  """The Computation Unit / Arithmetic-Logic Unit
+
+  Fields
+  ______
+  functs: The functions performed by the ALU (an opcode -> operation mapping)
+  endT: Time at which the execution is completed
+  opcode: The code of the operation currently being executed
+  res: The location where the result is to be stored
+  op1: The location of operator 1
+  op2: The location of operator 2
+
+  Methods
+  -------
+  reset: Reset the ALU
+  execute: Execute an operation
+  isBusy: Check if the ALU is busy
+  broadcast: Broadcast the output to the bus
+
+  """
 
   def __init__(self, functs):
     self.functs = functs
@@ -149,20 +254,37 @@ class ALU:
 
 
 class ReserSt:
+  """The Reservation Station Unit
+
+  Fields
+  ______
+  content: The content in the reservation station
+  t: The time fields : issue
+
+  Methods
+  -------
+  clear: Clear the contents of the reservation station
+  setr: Issue an instruction into the reservation station
+  capture: Capture the data from the bus into op1 and op2
+  free: Tag the reservation station as invalid
+  dispatch: Dispatch an instruction from the reservation station
+  getEntries: Return the description of the reservation station in a list (row)
+
+  """
 
   def __init__(self, idx=None):
     self.content = {
-      'idx': idx,
+        'idx': idx,
     }
     self.t = {}
     self.clear()
-    
+
   def clear(self):
     self.content['busy'] = 0
     cfields = ['disp', 'op', 'j', 'k']
     for field in cfields:
       self.content[field] = None
-    tfields = ['issue', 'dispatch']
+    tfields = ['issue']
     for field in tfields:
       self.t[field] = None
 
@@ -187,12 +309,13 @@ class ReserSt:
   def dispatch(self, alu):
     try:
       if self.content['j'][0] == 'ABS' and self.content['k'][
-          0] == 'ABS' and self.t['issue'] != currT and (self.content['disp'] is None):
+          0] == 'ABS' and self.t['issue'] != currT and (self.content['disp'] is
+                                                        None):
         self.content['disp'] = currT
         return (True,
                 alu.execute([
-                    self.content['op'], self.content['idx'], self.content['j'][1],
-                    self.content['k'][1]
+                    self.content['op'], self.content['idx'],
+                    self.content['j'][1], self.content['k'][1]
                 ]))
     finally:
       return (False, None)
@@ -207,11 +330,39 @@ class ReserSt:
           q[op] = c[op][1]
         else:
           v[op] = c[op][1]
-    
-    return remNone(['RS%d' % c['idx'], c['busy'], c['op'], v['j'], v['k'], q['j'], q['k'], c['disp']])
+
+    return remNone([
+        'RS%d' % c['idx'], c['busy'], c['op'], v['j'], v['k'], q['j'], q['k'],
+        c['disp']
+    ])
 
 
 class ReserALU:
+  """A Reservation Station Group & the respective ALU group
+
+  Fields
+  ______
+  tag: The tag of the Group : A -> Add M -> Mult
+  size: The size of the group of reservation station
+  RS: The list containing the group of Reservation Stations
+  alu: The assigned ALU for the group
+  valid: The list stating if an RS is busy or not
+  freed: The list stating the latest time an RS has been freed
+
+  Methods
+  -------
+  getUsage: An array contataing non-negative values:
+            0 denotes it can be allocated at currT
+  getr: Get the content of an RS
+  set: Set the content of an RS
+  isFull: Check if none of the RS in the group is available
+  capture: Capture the bus value into each RS
+  clear: Clear an RS
+  dispatch: Dispatch an RS into the ALU
+  brodcast: Broadcast the result of the ALU and the tag into the bus
+  getEntries: Return the description of the RS in a list of rows
+
+  """
 
   def __init__(self, tag, size, functs, st=0):
     self.tag = tag
@@ -265,31 +416,51 @@ class ReserALU:
 
 
 class ReserStGrp:
+  """The Group containg each type of Reservation Stations and their ALUs
+
+  Fields
+  ______
+  RG: The collection of (RS groups + ALU) of various types
+  types: The types of (RS groups + ALU) : A(ADD) and M(MULT)
+  rat: The RAT the whole RG points to
+
+  Methods
+  -------
+  getr: Get an RS by index
+  setr: Set an RS by index
+  isFull: Check if none of the RS in the group corresponding to the optype is available
+  capture: Capture the bus value into each RS
+  dispatch: Dispatch an RS into the ALU
+  broadcast: Broadcast the result of an ALU and its tag into the bus
+  
+  """
 
   def __init__(self, rat, A=3, M=2):
     self.RG = {
         'A':
-            ReserALU('A', A, {
-                0: {
-                    'funct': (lambda x, y: x + y),
-                    't': 2,
-                },
-                1: {
-                    'funct': (lambda x, y: x - y),
-                    't': 2,
-                },
-            }),
+            ReserALU(
+                'A', A, {
+                    0: {
+                        'funct': (lambda x, y: x + y),
+                        't': 2,
+                    },
+                    1: {
+                        'funct': (lambda x, y: x - y),
+                        't': 2,
+                    },
+                }),
         'M':
-            ReserALU('M', M, {
-                2: {
-                    'funct': (lambda x, y: x * y),
-                    't': 10,
-                },
-                3: {
-                    'funct': (lambda x, y: x / y),
-                    't': 40
-                },
-            }, A)
+            ReserALU(
+                'M', M, {
+                    2: {
+                        'funct': (lambda x, y: x * y),
+                        't': 10,
+                    },
+                    3: {
+                        'funct': (lambda x, y: x / y),
+                        't': 40
+                    },
+                }, A)
     }
     self.types = ['A', 'M']
     self.rat = rat
@@ -338,7 +509,7 @@ class ReserStGrp:
     for t in self.types:
       table.extend(self.RG[t].getEntries())
     return tabulate(table, headers, tablefmt='fancy_grid')
-  
+
   def __str__(self):
     return repr(self)
 
@@ -361,8 +532,9 @@ print('\n\n @@@ CLOCK CYCLE = 0 @@@\n\n')
 printState(rsg, rat, iq)
 
 for currT in range(1, T + 1):
-  print('\n\n @@@ CLOCK CYCLE = %d @@@\n\n' % (currT))
   
+  print('\n\n @@@ CLOCK CYCLE = %d @@@\n\n' % (currT))
+
   # issue
   if not iq.isEmpty():
     instr = iq.peek()
